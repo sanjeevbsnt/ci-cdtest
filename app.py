cbac96fda37d52968210d197e3d7de38405288bd
sanjeev <sanjeev.basnet@genesesolution.com>
2024-03-20 11:28:51 +0545

    Updated code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import datetime
-
 today = datetime.date.today()
 print("Today's date is:", today.strftime("%Y-%m-%d"))
+  print("This is a sample Python script for CI/CD testing.")  # Wrong indentation
+
