6838595ec0b530d7e43b7434f06159b392db992b
sanjeev <sanjeev.basnet@genesesolution.com>
2024-03-20 11:38:26 +0545

    corrected

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ birthday = datetime.date(year=2024, month=3, day=20)  # Adjust for your birthday
 
 today = datetime.date.today()
 
-if today = birthday:  # Assignment instead of comparison
+if today == birthday:
     print("Happy Birthday!")
 else:
     print("Today is not your birthday.")
